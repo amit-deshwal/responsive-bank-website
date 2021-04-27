34e73289de9c837eaa05aa809ef033620165aa86
amit-deshwal <a3deswal@gmail.com>
2021-04-28 03:55:23 +0530

    Completed all the sections & made it responsive across different screen sizes

---
+++ app/js/script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector('body');
+const btnHamburger = document.querySelector('#btnHamburger')
+const header = document.querySelector('.header') 
+const overlay = document.querySelector('.overlay');
+const menuItemsMobile = document.querySelector('.header__menu');
+console.log(menuItemsMobile);
+
+btnHamburger.addEventListener('click',function(){
+    body.classList.toggle('no-scroll')
+    header.classList.toggle('open')
+    overlay.classList.toggle('fade-in')
+    overlay.classList.toggle('fade-out',!overlay.classList.contains('fade-in'))
+    menuItemsMobile.classList.toggle('fade-in')
+    menuItemsMobile.classList.toggle('fade-out',!menuItemsMobile.classList.contains('fade-in'))
+    menuItemsMobile.classList.toggle('has-fade',!menuItemsMobile.classList.contains('fade-in'))
+}) 
